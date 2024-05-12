0da6005ab4f743c86d5a30340f37a1411628f99c
seniordeveloperpy <kamronbekmubinaxon18@gmail.com>
2024-05-12 20:42:07 +0500

    first commit

---
+++ main/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Home(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    img = models.ImageField(upload_to='home/')
+
+
+class Portfolio(models.Model):
+    title = models.CharField(max_length=255)
+    img = models.ImageField(upload_to='portfolio/')
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    user_img = models.ImageField(upload_to='team/')
+    username = models.CharField(max_length=255)
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=13)
+    about_project = models.TextField(blank=True, null=True)
+
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=255)
+    major = models.CharField(max_length=255)
+    grafik = models.IntegerField()
+    start_work = models.DateTimeField(auto_now_add=True)
+    end_work = models.DateTimeField(auto_now_add=True)
+    salary = models.IntegerField()
+    requirements = models.TextField()
+    tasks = models.TextField()
+    texnology = models.CharField(max_length=255)
+    fullname = models.CharField(max_length=255)
+    phone = models.CharField(max_length=13)
+    resume = models.FileField(upload_to='vacancy/')
